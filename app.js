5f4507f2910aaa5d1663fb3667d64025e0491802
hayatespinner <white8810wnew@gmail.com>
2020-06-13 15:18:25 +0900

    m

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 
 //// CSSや画像ファイルを置くフォルダを指定する部分
 app.use(express.static('public'));
+
+//mysqlの接続情報
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '881010',
+  database: 'progate'
+});
+
+//エラーの時の表示
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('success');
+});
+
+
 
 //topを表示
 app.get("/", (req,res)=>{
@@ -10,13 +30,15 @@ app.get("/", (req,res)=>{
 });
 
 //indexを表示
-app.get("/index", (req,res)=>{
-  res.render("index.ejs");
+app.get('/index', (req, res) => {
+  // データベースからデータを取得する処理
+  connection.query(
+    "SELECT * FROM items",
+    (error, results) => {
+      console.log(results);
+      res.render('index.ejs');
+    }
+  );
 });
 
-// サーバーを起動する部分（＊固定）
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+app.listen(3000);
