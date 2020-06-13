2c3e28ff7fae851627d6dbebfd20ef71ee83bee8
hayatespinner <white8810wnew@gmail.com>
2020-06-13 10:08:36 +0900

    m

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+//// CSSや画像ファイルを置くフォルダを指定する部分
+app.use(express.static('public'));
+
 // HTTPリクエストを受け取る部分
 app.get('/', (req, res) => {
   res.render("hello.ejs");
@@ -11,7 +14,12 @@ app.get("/top", (req,res)=>{
   res.render("top.ejs");
 });
 
-// サーバーを起動する部分
+//indexを表示
+app.get("/index", (req,res)=>{
+  res.render("index.ejs");
+});
+
+// サーバーを起動する部分（＊固定）
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
